f7b9d94231e3d5426c43de07bad51bced720d1b2
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-03-21 20:18:47 +0000

    Dropdown menu
    it was planned from the beginning, it is a dropdown menu to sort the music by music style, artist or geographic location, it will be improved for be more intituive and attractive

--- js/MuCr.js
+++ js/MuCr.js
@@ -1,9 +1,20 @@
-var visto = null;
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-function mostrar(num) {
-	obj = document.getElementById(num);
-	obj.style.display = (obj == visto) ? 'none' : 'block';
-	if (visto != null)
-		visto.style.display = 'none';
-	visto = (obj == visto) ? null : obj;
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
