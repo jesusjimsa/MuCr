fbf04049f39a5a36c942782edd72c8fdfb031b96
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-03-21 21:48:24 +0000

    login view change
    The background images changes randomly and that include changes like the  info which appear on the right side which update

--- js/MuCr.js
+++ js/MuCr.js
@@ -18,3 +18,23 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+var fondos = ["../img/backgrounds/welcome/fondo1.jpg","../img/backgrounds/welcome/fondo2.jpg","../img/backgrounds/welcome/fondo3.jpg","../img/backgrounds/welcome/fondo4.jpg","../img/backgrounds/welcome/fondo5.jpg","../img/backgrounds/welcome/fondo6.jpg"]
+var informa=["Palau de la Música Catalana, Spain","Teatro Romano de Mérida, Spain","Dreambeach Festival in Almeria, Spain","Tomorrowland in BOOM, Belgica", "Opera House in Sydney, Australia","Opera House in Vienna, Austria"]
+
+function cambiarnombre(numero){
+	var aux=informa[numero]
+	document.getElementById("info").innerHTML=aux
+}
+
+function fondoBody() {
+	var numero=Math.floor(Math.random()*fondos.length)
+cambiarnombre(numero)
+var image = fondos[numero]
+document.body.style.backgroundImage="url("+image+")";
+ document.body.style.backgroundRepeat="no-repeat";
+ document.body.style.backgroundSize="cover";
+ document.body.style.backgroundPosition="bottom center";
+ document.body.style.backgroundAttachment="fixed";
+}
