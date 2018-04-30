f1fb3410cb565ffa3349d2474d37b9b5643afaaf
Jose Manuel Rodriguez Calvo <jmr_calvo@outlook.com>
2018-04-30 13:37:43 +0300

    error python, confusing javscript and finally get back to php
    this was all what I have to do to be able to publish our webpage locally correct

--- identificacion.php
+++ identificacion.php
@@ -1,15 +1,35 @@
 <?php
+$email=$_POST['email'];
+$password=$_POST['password'];
 
-$email=$password=$password2=NULL;
+$servername = "db4free.net";
+$username = "jmrodriguez";
+$password = "9uk6vw6e";
+$dbname = "orfeo_mucr";
 
-$email=$_GET["email"];
-$password=$_GET["password"];
-$password2=$_GET["password2"];
-
-if(password2=="NULL"){
-  $command="python back-end.py $email $password";
-  $pid = popen( $command,"r");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT email, password FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+	 while($row = mysqli_fetch_assoc($result)) {
+		if($row["email"]==$email){
+			if($row["password"]==$password){
+				header('Location: MuCr_main.html');}
+			}
+		}
+}
+$conn->close();
+
+
+
 
 
 ?>
