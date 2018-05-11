aca841307e98bbdd5beff50c3517a7e413b48239
jesusjimsa <jesusjimsa@icloud.com>
2018-05-11 16:00:36 +0300

    Delete account

--- php/delete_account.php
+++ php/delete_account.php
@@ -1,30 +1,29 @@
 <?php
 $email = $_COOKIE['email'];
 
-	include 'connect_db.php';	// Connect to database
+include 'connect_db.php';	// Connect to database
 
-	$sql_check_email = "SELECT email FROM user WHERE '$email' = email" ;
-	$result = $conn->query($sql_check_email);
+$sql_check_email = "SELECT email FROM user WHERE '$email' = email" ;
+$result = $conn->query($sql_check_email);
+// var_dump($result);
 
-	if($result->num_rows == 0){
-		$sql_insert_user = "DELETE user WHERE email = '$email'";
+if($result->num_rows > 0){
+	$sql_insert_user = "DELETE FROM user WHERE email = '$email'";
 
-		if ($conn->query($sql_insert_user) == TRUE){
-			include 'delete_cookies.php';
+	if ($conn->query($sql_insert_user) == TRUE){
+		include 'delete_cookies.php';
 
-			header('Location: ../index.html');
-			echo "Eliminated successfully";
-		}
-		else{
-			// header('Location: index.html');
-			echo "Error: " . $sql_insert_user . "<br>" . $conn->error;
-		}
+		header('Location: ../index.html');
+		echo "Deleted successfully";
 	}
 	else{
-		echo "hola";
-		header('Location: ../index.html');
+		// header('Location: index.html');
+		echo "Error: " . $sql_insert_user . "<br>" . $conn->error;
 	}
+}
+else{
+	header('Location: ../MuCr_settings.html');
+}
 
-	$conn->close();
-
+$conn->close();
 ?>
