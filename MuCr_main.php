74ff0c9287fd9669a10e797618f422859a953ae8
jesusjimsa <jesusjimsa@icloud.com>
2018-05-22 01:29:01 +0300

    Change to file

--- MuCr_main.php
+++ MuCr_main.php
@@ -19,20 +19,7 @@
 			</div>
 			<div class="titulo">
 				<?php
-					include 'php/connect_db.php';
-
-					$email = $_COOKIE['email'];
-
-					$sql_query = "SELECT email FROM user WHERE email = '$email'";
-
-					$result = mysqli_query($conn, $sql_query);
-					$row = mysqli_fetch_assoc($result);
-
-					list($username, $else) = explode('@', $row['email']);
-
-					echo $username;
-					ob_flush();
-					flush();
+					include 'php/write_username.php'
 				?>
 			</div>
 		</div>
