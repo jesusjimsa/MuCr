4e2f2a76aa85a3a6fc482efccf21eef62a689e64
jesusjimsa <jesusjimsa@icloud.com>
2018-05-24 17:03:20 +0300

    Not needed files

--- back-end.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import urllib2
-import SimpleHTTPServer
-import SocketServer
-
-httpd = SocketServer.TCPServer(("127.0.0.1", 9000), SimpleHTTPServer.SimpleHTTPRequestHandler)
-
-httpd.serve_forever()
