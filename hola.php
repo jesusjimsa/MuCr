79bc237d1929e96e6840851a9b866dd71f6cf40f
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-06-03 17:44:14 +0300

    controller of the lists
    controller of the lists in the main

--- hola.php
+++ hola.php
@@ -1,17 +1,17 @@
 <?php
-	include 'controladorAlbumRelacionado.php';
-	include 'php/connect_db.php';
-
-	$lll = new album;
-	$lll->createAlbumsearched("Michael Jackson", "Thriller 25 ");
-	//echo "hola";
-	$titulo=$lll->getTitulo();
-	$artista=$lll->getArtista();
-	echo $lll->getGenres();
-	$year=$lll->getYear();
-	$type=$lll->getType();
-	$deluxe=$lll->getDeluxe();
-	$sql_order = "INSERT INTO album(name,artist) VALUES ('$titulo', '$artista')";	// $url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" . "7469e83d1fc6415f3a1cf8790bdebacd". "&artist=" . urlencode("mecano") . "&album=" . urlencode("aidalai") . "&format=json";
+	// include 'controladorAlbumRelacionado.php';
+	// include 'php/connect_db.php';
+	//
+	// $lll = new album;
+	// $lll->createAlbumsearched("Michael Jackson", "Thriller 25 ");
+	// //echo "hola";
+	// $titulo=$lll->getTitulo();
+	// $artista=$lll->getArtista();
+	// echo $lll->getGenres();
+	// $year=$lll->getYear();
+	// $type=$lll->getType();
+	// $deluxe=$lll->getDeluxe();
+	// $sql_order = "INSERT INTO album(name,artist) VALUES ('$titulo', '$artista')";	// $url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" . "7469e83d1fc6415f3a1cf8790bdebacd". "&artist=" . urlencode("mecano") . "&album=" . urlencode("aidalai") . "&format=json";
 	//
 	// //get the ID
 	// $ch = curl_init();
@@ -25,4 +25,8 @@
 	// $response = json_decode($response, JSON_FORCE_OBJECT);
 	//
 	//  var_dump($response["album"]);
+	include 'listController.php';
+
+	$lista1 = new lista("noasda","nada","rodrigranada97@gmail.com");
+//	echo $lista1->searchtopic2("rodrigranada97@gmail.com");
 ?>
