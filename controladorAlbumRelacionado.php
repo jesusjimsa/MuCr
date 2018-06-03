4293d5636362f411e0ff8f89007f653720c5f05a
Jose Manuel Rodriguez Calvo <34111274+jmrCalvo@users.noreply.github.com>
2018-06-03 20:18:51 +0300

    changes to jesuss branch
    changes to get the branch of jesus

--- controladorAlbumRelacionado.php
+++ controladorAlbumRelacionado.php
@@ -40,7 +40,6 @@ class album{
 	}
 	public function getYear(){
 		return $this->year;
-
 	}
 
 	public function getDeluxe(){
@@ -85,13 +84,14 @@ class album{
 
 	public function addAlbumtoBd(){
 		include 'php/connect_db.php';
-		$year = $this->getYear();
 
+		$year = $this->getYear();
 		$deluxe = $this->getdeluxe();
 		$type = $this->getType();
 		$artist = $this->getArtista();
 		$titulo = $this->getTitulo();
 		$style=$this->getGenres();
+
 		$sql_order = "INSERT INTO album(name,artist,style,type,deluxe,year) VALUES ('$titulo', '$artist','$style','$type','$deluxe','$year')";
 		$conn->query($sql_order);
 	}
@@ -119,82 +119,93 @@ class album{
 		else{return false;}
 	}
 
-
 	public function setGenress($listas){
-			$tagss="";
-			$style=array("alternative","punk","rock","blues","classical","country","folk","dance","electronic","easy","gospel","religious","rap","holiday","instrumental","jazz","latin","metal","moods","pop","rnb","soundtrack","world");
-			$max=count($listas);
+			$tagss = "";
+			$max = count($listas);
+			$style = array(
+				"alternative","punk","rock","blues",
+				"classical","country","folk","dance",
+				"electronic","easy","gospel","religious",
+				"rap","holiday","instrumental","jazz",
+				"latin","metal","moods","pop","rnb",
+				"soundtrack","world"
+			);
 
 			for($i=0;$i<$max;$i++){
-
-				//var_dump($selected);
-					//echo $listas[$i]["name"];
-					if($listas[$i]["name"]==="60s"){$tagss.="60s"."-";}
-						else{
-								if($listas[$i]["name"]==="70s"){$tagss.="70s"."-";}
-								else{
-										if($listas[$i]["name"]==="80s"){$tagss.="80s"."-";}
-										else{
-												if($listas[$i]["name"]==="90s"){$tagss.="90s"."-";}
-												else{
-														foreach($style as $styleSelected){
-															if($this->isContained($listas[$i]["name"], $styleSelected)){
-																	$tagss.=$styleSelected."-";
-															}
-														}
-													}
-											}
-										}
-									}
+				if($listas[$i]["name"] === "60s"){
+					$tagss.="60s"."-";
+				}
+				else{
+					if($listas[$i]["name"] === "70s"){
+						$tagss.="70s"."-";
+					}
+					else{
+						if($listas[$i]["name"] === "80s"){
+							$tagss.="80s"."-";
 						}
-			if($tagss===""){
-				$tagss="Other";
+						else{
+							if($listas[$i]["name"] === "90s"){
+								$tagss.="90s"."-";
+							}
+							else{
+								foreach($style as $styleSelected){
+									if($this->isContained($listas[$i]["name"], $styleSelected)){
+										$tagss.=$styleSelected."-";
+									}
+								}
+							}
+						}
+					}
+				}
 			}
+			
+			if($tagss === ""){
+				$tagss = "Other";
+			}
+			
 			return $tagss;
 	}
 
-public function getTypeandYear($artista,$albumtitle){
-				$pos=strpos($albumtitle,"(");
-				//echo $pos;
-				$titulo="";
-				if($pos===false){$titulo=$albumtitle;}
-				else{$titulo=substr($albumtitle,0,$pos-1);}
-				//echo $titulo;
+	public function getTypeandYear($artista,$albumtitle){
+		$pos = strpos($albumtitle,"(");
+		$titulo = "";
 
-				$fmt = 'json';
-				$url = "http://musicbrainz.org/ws/2/release/?query=artist:" . urlencode($artista) . "&fmt=".$fmt;
-				//get the ID
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
-				$response = curl_exec($ch);
-				// var_dump($response);
-				curl_close($ch);
-				$response = json_decode($response, JSON_FORCE_OBJECT);
-				$i = 0;
-				// echo $albumtitle;
+		if($pos === false){
+			$titulo = $albumtitle;
+		}
+		else{
+			$titulo = substr($albumtitle, 0, $pos - 1);
+		}
 
-				while($response["releases"][$i]["title"] != $titulo && $i <(count($response["releases"])-1) ){
-					$i++;
-				}
+		$fmt = 'json';
+		$url = "http://musicbrainz.org/ws/2/release/?query=artist:" . urlencode($artista) . "&fmt=".$fmt;
+		
+		//get the ID
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
+		$response = curl_exec($ch);
 
+		curl_close($ch);
+		$response = json_decode($response, JSON_FORCE_OBJECT);
+		$i = 0;
 
-				if($response["releases"][$i]["title"] == $titulo){
-						$this->type = $response["releases"][$i]["media"][0]["format"];
-						$this->deluxe = $this->isDeluxe($albumtitle);
-						$this->year=substr($response["releases"][$i]["release-events"][0]["date"],0,4);
-				}
-				else{
-					$this->type = NULL;
-					$this->year=NULL;
-					$this->deluxe=NULL;
-				}
-			}
+		while($response["releases"][$i]["title"] != $titulo && $i <(count($response["releases"])-1) ){
+			$i++;
+		}
 
-
-
-
+		if($response["releases"][$i]["title"] == $titulo){
+			$this->type = $response["releases"][$i]["media"][0]["format"];
+			$this->deluxe = $this->isDeluxe($albumtitle);
+			$this->year=substr($response["releases"][$i]["release-events"][0]["date"],0,4);
+		}
+		else{
+			$this->type = NULL;
+			$this->year=NULL;
+			$this->deluxe=NULL;
+		}
+	}
 
 	public function createAlbumRand($artista1){
 		$url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" . $artista1 . "&api_key=" . $this->API_KEY . "&format=json";
@@ -221,9 +232,6 @@ public function getTypeandYear($artista,$albumtitle){
 
 		$this->genres = $this->setGenress($response["album"]["tags"]["tag"]);
 		$this->getTypeandYear($this->artista,$this->titulo);
-		//$this->type = $response["releases"][$numer]["media"][0]["format"];
-		//$this->deluxe = 0;//$this->isDeluxe($response["releases"][$numer]["artist-credit"][0]["artist"]["disambiguation"]);
-		//$this->year = substr($response["releases"][$numer]["release-events"][0]["date"],0,4);
 		$this->addAlbumtoBd();
 
 		return $this;
@@ -239,12 +247,11 @@ public function getTypeandYear($artista,$albumtitle){
 		curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
 
 		$response = curl_exec($ch);
-		// var_dump($response);
+
 		curl_close($ch);
 		$response = json_decode($response, JSON_FORCE_OBJECT);
 
-
-			$this->id = $response["album"]["mbid"];
+		$this->id = $response["album"]["mbid"];
 
 		//get the title
 		$this->titulo = $response["album"]["name"];
@@ -253,20 +260,54 @@ public function getTypeandYear($artista,$albumtitle){
 		$this->artista = $response["album"]["artist"];
 		$this->genres=$this->setGenress($response["album"]["tags"]["tag"]);
 		$this->getTypeandYear($this->artista,$this->titulo);
-		// $this->type = $response["releases"][$i]["media"][0]["format"];
-		// $this->deluxe =0; //$this->isDeluxe($response["releases"][$i]["artist-credit"][0]["artist"]["disambiguation"]);
-		// $this->year=substr($response["releases"][$i]["release-events"][0]["date"],0,4);
 		$this->addAlbumtoBd();
 
 		return $this;
 	}
 
+	public function printSongs(){
+		$url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" . $this->API_KEY . "&artist=" . $this->artista . "&album=" . $this->titulo . "&format=json";
 
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
 
+		$response = curl_exec($ch);
 
+		curl_close($ch);
+		$response = json_decode($response, JSON_FORCE_OBJECT);
 
+		$song_list = $response["album"]["tracks"]["track"];
 
+		for($i = 0; $i < count($song_list); $i++){
+			$time_min = (int)((int)$song_list[$i]["duration"] / 60);
+			$time_sec = (int)$song_list[$i]["duration"] - ($time_min * 60);
 
+			echo "<div class=\"songs\">" . $song_list[$i]["name"] . " - " . $time_min . ":" . $time_sec . "\n<div class=\"love_song\">\n<img src=\"img/icons/heart.png\" alt=\"love_song\">\n</div>\n</div>\n";
+		}
+	}
+
+	public function createAlbumRandByTag($tag){
+		$url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=" . $tag . "&api_key=" . $API_KEY . "&format=json";
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+		$response = json_decode($response, JSON_FORCE_OBJECT);
+
+		$top_albums_tag = $response["albums"]["album"];
+
+		$rand_num = rand(0, count($top_albums_tag));
+
+		$this->titulo = $top_albums_tag[$i]["name"];
+		$this->artista = $top_albums_tag[$i]["artist"]["name"];
+		$this->id = $top_albums_tag[$i]["mbid"];
+	}
 }
 
 ?>
