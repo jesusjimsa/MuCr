d4641e75716a5440ad47a4b1f1cccfe8bdec5507
jesusjimsa <jesusjimsa@icloud.com>
2018-06-04 12:57:45 +0300

    recursivamente harto

--- hola.php
+++ hola.php
@@ -1,12 +1,9 @@
 <?php
   	include 'listController.php';
 
-
-
-
   	$added=array();
- 	$lista1 = new lista("artistvalored",$added,"rodrigranada97@gmail.com");
+ 	$lista1 = new lista("artistvalored", $added, "rodrigranada97@gmail.com");
   	// echo $lista1->searchtopic2("rodrigranada97@gmail.com");
-  	var_dump($lista1->getAlbumV1);
-
+  	// var_dump($lista1->getAlbumV1);
+	echo "hoal";
   ?>
