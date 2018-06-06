8ec654cee2c3668becb9801948510bc399d5332b
jesusjimsa <jesusjimsa@icloud.com>
2018-06-06 22:58:59 +0300

    Fill concerts by artist and country

--- hola.php
+++ hola.php
@@ -2,10 +2,6 @@
 	include 'php/fill_concert.php';
 
 	
-
-	if($_GET['country'] != ""){
-		echo $_GET['artist'];
-	echo $_GET['country'];
-		printConcerts($_GET['artist'], $_GET['country']);
-	}
+	printConcerts("Elton John", "GB");
+	
 ?>
