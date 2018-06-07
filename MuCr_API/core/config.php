3d70acf721536c75bace30bc60af5ba76ae34b5e
jesusjimsa <jesusjimsa@icloud.com>
2018-06-07 10:09:09 +0300

    API

---
+++ MuCr_API/core/config.php
@@ -0,0 +1,13 @@
+<?php 
+	// Database server url
+	define("DB_HOST", "db4free.net");
+
+	// Database username
+	define("DB_USER", "jmrodriguez");
+
+	// Database password
+	define("DB_PASS", "9uk6vw6e");
+
+	// Table name
+	define("DB_NAME", "orfeo_mucr");
+?>
